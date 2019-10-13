4a2b071f0bc12e5b3f69a864b0684bbeec66d91c
Makeda P Mckune <makedamckune@gmail.com>
2019-10-13 00:54:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  
+def key_for_min_value
+  number = [30,15,0,56,-2,54]
+      number.smallest do |i|
+  end
 
 end
