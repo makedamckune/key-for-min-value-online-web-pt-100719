569edde4ebcc23371d3784075f16e604f9a065c8
Makeda P Mckune <makedamckune@gmail.com>
2019-10-13 00:59:35 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,10 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value
+ a = [2,5,6,8,3,4,0,9]
+min = a.min
+a.select { |i| i == min }
+ 
+ 
+  end
 
 end
